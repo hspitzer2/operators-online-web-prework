9c8f832a4265cccec9187c6867acab3f7a921b1e
Hillary Spitzer <hspitzer2@gmail.com>
2019-05-29 23:12:58 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,6 +1,7 @@
 
 unsafe = true
 def unsafe?(speed)
+<<<<<<< HEAD
   if speed < 40
    true
   elsif speed > 60
@@ -12,5 +13,17 @@ end
 
 def not_safe?(speed)
     speed > 60 || speed < 40 ? true : false
+=======
+  if speed > 60 || speed < 40
+   return true
+  else
+   return false
+  end
+end
+
+def not_safe?(speed)
+   speed  > 60 || < 40 ? true : false
+end
+>>>>>>> ae6bede9bfa959535d897d10bb4946542818f628
 
 end
